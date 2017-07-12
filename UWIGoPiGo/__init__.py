89c015837985b5b89e7e00badc6839f983a364ff
InzamamRahaman <izzyrahaman@gmail.com>
2017-07-12 15:22:38 -0400

    init

---
+++ UWIGoPiGo/__init__.py
@@ -0,0 +1,128 @@
+import easygopigo3 as easy
+import gopigo3 as go
+from __future__ import print_function
+from six.moves import input
+import time
+
+
+ROBOT = easy.EasyGoPiGo3()
+MAX_INT = 10000
+SENSOR = None
+SERVO = None
+SLEEP_TIME = 0.3
+PORT = 'I2C1'
+
+def measure():
+    """
+    Uses the distance sensor to measure the distance from the robot to the nearest object
+    with the sensor's scope
+    :return: the distance to the nearest object in centimetres
+    """
+    global ROBOT
+    global SENSOR
+    # if the sensor has not been initialised yet
+    if not SENSOR:
+        SENSOR = easy.DistanceSensor(PORT, ROBOT)
+    distance = SENSOR.read()
+    return distance
+
+def get_robot():
+    """
+    Grabs the robot object (breaks encapsulation :( )
+    :return: None
+    """
+    global ROBOT
+    return ROBOT
+
+def go_forward(distance):
+    """
+    Makes the robot go forward a specified distance
+    :param distance: the distance to travel in centimetres
+    :return: None
+    """
+    global ROBOT
+    ROBOT.drive_cm(distance)
+
+
+def go_backwards(distance):
+    """
+    Makes the robot go backwards a specified distance
+    :param distance: the distance to travel in centimetres
+    :return: None
+    """
+    global ROBOT
+    ROBOT.drive_cm(-1 * distance)
+
+
+def rotate_right(angle=90):
+    """
+    Makes the robot rotate in a right direction by a specified angle
+    :param angle: the angle to rotate by in degrees
+    :return: None
+    """
+    global ROBOT
+    ROBOT.turn_degrees(angle)
+
+
+
+def rotate_left(angle=90):
+    """
+    Makes the robot rotate in a left direction by a specified angle
+    :param angle: the angle to rotate by in degrees
+    :return: None
+    """
+    global ROBOT
+    ROBOT.turn_degrees(-1 * angle)
+
+
+def set_speed(speed):
+    """
+    Changes the current speed of the robot
+    :param speed: the new speed
+    :return: None
+    """
+    global ROBOT
+    ROBOT.set_speed(speed)
+
+
+def get_speed():
+    """
+    Gets the speed from the robot
+    :return: the current speed of the robot
+    """
+    global ROBOT
+    ROBOT.get_speed()
+
+
+def march_forward(braking_distance=MAX_INT):
+    """
+    Causes the robot to move forwards until it reaches with a specified distance of an arbitray object
+    :param braking_distance: the minimum distance between the robot and object (optional: default ignore obstacles)
+    :return: None
+    """
+    global ROBOT
+    distance = measure()
+
+    while distance > braking_distance:
+        ROBOT.forward()
+        time.sleep(SLEEP_TIME)
+        distance = measure()
+
+
+def stop():
+    """
+    Stops the robot
+    :return: None
+    """
+    global ROBOT
+    ROBOT.stop()
+
+
+def reverse():
+    """
+    Reverses the robot
+    :return: None
+    """
+    global ROBOT
+    ROBOT.backward()
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='UWIGoPiGo',
+    version='0.1dev',
+    packages=['UWIGoPiGo'],
+    license='MIT',
+    long_description='UWI GoPiGo Wrapper'
+)
